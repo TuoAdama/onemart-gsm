490b8b8fb08820d15c246cf1367ef9e6e7464cf1
TuoAdama <tuoadama17@gmail.com>
2022-07-22 17:56:19 +0000

    Transfert actions logs

--- app/Http/Controllers/TransfertController.php
+++ app/Http/Controllers/TransfertController.php
@@ -124,23 +124,25 @@ class TransfertController extends Controller
     {
         Transfert::where('etat_id', Etat::ECHOUE)
         ->update(['etat_id' => Etat::EN_COURS]);
-
+        info("Transferts echoués ont été relancés");
         return redirect()->back()->with("relaunch", 'Transferts relancés.');
     }
-
+    
     public function cancel()
     {
         Transfert::where('etat_id', Etat::ECHOUE)
         ->update(['etat_id' => Etat::ANNULER]);
-
+        
+        info("Transferts echoués ont été annulés");
         return redirect()->back()->with("cancel", 'Transferts annulés.');
     }
-
+    
     public function annulerTransfertEncours()
     {
         Transfert::where('etat_id', Etat::EN_COURS)
         ->update(['etat_id' => Etat::ANNULER]);
-
+        
+        info("Transferts en cours ont été annulés");
         return redirect()->back()->with("cancel", 'Transferts annulés.');
     }
 }
